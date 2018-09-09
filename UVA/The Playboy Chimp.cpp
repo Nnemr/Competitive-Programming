4bd4aa46f70c1cefc36b1a8e014b1c058437bf5e
Youssef Alnemr <20484975+Nnemr@users.noreply.github.com>
2018-09-09 03:25:04 -0700

    Create The Playboy Chimp.cpp

---
+++ UVA/The Playboy Chimp.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int n;
+	scanf("%d", &n);
+	int chimps[(int)5e5];
+	int ind =0;
+	for(int i=0; i<n;++i){
+		int x;
+		scanf("%d",&x);
+		if(ind){
+			if(chimps[ind-1] ==x)
+				continue;
+			chimps[ind++]=x;
+		}
+		chimps[ind++]=x;
+	}
+	int q;
+	scanf("%d", &q);
+	while(q--){
+		int x;
+		scanf("%d", &x);
+		auto low = lower_bound(chimps, chimps+ind, x), hi = upper_bound(chimps,chimps+ind, x);
+		--low;
+		if(low == ar-1){
+			printf("X ");
+		}
+		else
+			printf("%d ", *low);
+		if(hi  == ar+ind)
+			puts("X");
+		else
+			printf("%d\n", *hi);
+	}
+	return 0;
+}
